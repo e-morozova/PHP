e59960fcf4261281b5e0e31b59b52a64b807e4c4
e-morozova <m_e_v@rambler.ru>
2019-08-05 23:54:50 +0300

    добавлено задание №2

--- les2.php
+++ les2.php
@@ -10,15 +10,35 @@
 	} elseif (($a >= 0 && $b < 0) || ($a < 0 && $b >= 0)) {
 	    echo 'Сумма чисел равна: '.($a + $b);
     }
-
+*/
+/*
 // задание №2
-	
-
+	$a = 5;
+	switch ($a) {
+		case 1:
+			echo "1";
+		case 2:
+			echo "2";
+		case 3:
+			echo "3";
+		case 4:
+			echo "4";
+		case 5:
+			echo "5";
+		case 6:
+			echo "6";
+		case 7:
+			echo "7";
+		case 8:
+			echo "8";
+		break;
+	}
+*/
 
 
 // задание №3
 
-*/
+/*
 // задание №4
 	function mathOperation($arg1, $arg2, $operation) {
 	switch ($operation) {
@@ -37,7 +57,7 @@
 		}
 	}
 	echo mathOperation(10, 0, divide);
-	
+	*/
 
 
 
